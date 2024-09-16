0893b8613ca91a3b5718de28d1f0188f355697bf
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-09-16 22:40:37 +0700

    chore: update badge scripts

--- scripts/badge.js
+++ scripts/badge.js
@@ -2,16 +2,17 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const fetchOrgGitHubRepoData = require("./fetch.js");
+const { optimizeSvg, prettierSvg } = require("./formattedSVG.js");
 
 // File name for the badges
 const starsBadgeFile = path.join(__dirname, "../images/badges/stars-count.svg");
 const reposBadgeFile = path.join(__dirname, "../images/badges/repos-count.svg");
 
 (async () => {
-    const { totalStars, totalPublicRepos } = await fetchOrgGitHubRepoData("BarudakRosul");
-    const starsBadge = await axios.get(`https://img.shields.io/badge/star%27s%20count-${totalStars}-orange?&style=for-the-badge`);
-    const reposBadge = await axios.get(`https://img.shields.io/badge/repo%27s%20count-${totalPublicRepos}-red?&style=for-the-badge`);
+    const fetch = await fetchOrgGitHubRepoData("BarudakRosul");
+    const starsBadge = await axios.get(`https://img.shields.io/badge/star%27s%20count-${fetch.stars}-orange?&style=for-the-badge`);
+    const reposBadge = await axios.get(`https://img.shields.io/badge/repo%27s%20count-${fetch.repos}-red?&style=for-the-badge`);
 
-    fs.writeFileSync(starsBadgeFile, starsBadge.data);
-    fs.writeFileSync(reposBadgeFile, reposBadge.data);
+    fs.writeFileSync(starsBadgeFile, await prettierSvg(optimizeSvg(starsBadge.data)));
+    fs.writeFileSync(reposBadgeFile, await prettierSvg(optimizeSvg(reposBadge.data)));
 })();
