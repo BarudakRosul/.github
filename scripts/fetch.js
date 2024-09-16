b16b81cfeb17fa8d9a307b62607974fefe4707ee
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-09-16 22:45:36 +0700

    chore: update fetch scripts

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,27 +1,22 @@
 const axios = require("axios");
 
 async function fetchOrgGitHubRepoData(org) {
-    try {
-        const response = await axios.get(`https://api.github.com/orgs/${org}/repos`);
-        const repos = response.data;
+    const response = await axios.get(`https://api.github.com/orgs/${org}/repos`);
+    const repos = response.data;
 
-        let totalStars = 0;
-        let totalPublicRepos = 0;
+    let totalStars = 0;
+    let totalPublicRepos = 0;
 
-        for (const repo of repos) {
-            if (repo.name !== '.github') {
-                if (!repo.private) {
-                    totalPublicRepos += 1;
-                }
-                totalStars += repo.stargazers_count;
+    for (const repo of repos) {
+        if (repo.name !== '.github') {
+            if (!repo.private) {
+                totalPublicRepos += 1;
             }
+            totalStars += repo.stargazers_count;
         }
-
-        return { totalStars, totalPublicRepos };
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return { totalStars: 0, totalPublicRepos: 0 };
     }
+
+    return { stars: totalStars, repos: totalPublicRepos };
 }
 
 module.exports = fetchOrgGitHubRepoData;
